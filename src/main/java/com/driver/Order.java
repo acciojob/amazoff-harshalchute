1472debe1f1e4d4591cb2e8dd7413d8dd4c9f875
Harshalchute <harshalchute1@gmail.com>
2024-03-16 20:08:54 +0530

    Attempt 3

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,13 +9,18 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.id=id;
+
+        String[] time = deliveryTime.split(":");
+        int hours = Integer.parseInt(time[0]);
+        int minutes = Integer.parseInt(time[1]);
+        // Convert the time to minutes
+        this.deliveryTime = (hours * 60) + minutes;
     }
 
     public String getId() {
         return id;
     }
 
-    public int getDeliveryTime() {
-        return deliveryTime;
-    }
+    public int getDeliveryTime() {return deliveryTime;}
 }
